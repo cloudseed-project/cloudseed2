ffca17d5a9012f7a148db0d5839900ccdb6deb24
Adam Venturella <aventurella@gmail.com>
2013-06-07 07:36:49 -0700

    alternate invocation

--- cloudseed/agent/main.py
+++ cloudseed/agent/main.py
@@ -6,11 +6,16 @@ from .agent import salt_master_events
 
 
 def main():
+    p = Process(target=_start_daemon)
+    p.start()
+    p.join()
+
+
+def _start_daemon():
+    daemonize()
     Process(name='cloudseed worker', target=worker).start()
     Process(name='cloudseed agent', target=agent).start()
     Process(name='cloudseed salt events', target=salt_master_events).start()
-    daemonize()
-
 
 if __name__ == '__main__':
     main()
