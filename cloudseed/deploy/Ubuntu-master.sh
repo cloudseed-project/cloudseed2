7788e87e2ad93e544c95b2d94e8000112bea2751
Adam Venturella <aventurella@gmail.com>
2013-06-10 08:33:47 -0700

    deploy

--- cloudseed/deploy/Ubuntu-master.sh
+++ cloudseed/deploy/Ubuntu-master.sh
@@ -18,7 +18,7 @@ pip install gitpython salt-cloud apache-libcloud pymongo
 # debugging purposes:
 # this will be a pip install
 # must start before the master comes online
-sudo sh -c "git clone https://github.com/cloudseed-project/cloudseed2.git ~/cloudseed;  python ~/cloudseed/setup.py develop; cloudseed agent"
+sudo sh -c "git clone https://github.com/cloudseed-project/cloudseed2.git ~/cloudseed; cd ~/cloudseed && python setup.py develop; cloudseed agent"
 
 apt-get install -y -o DPkg::Options::=--force-confold salt-master
 salt-key --gen-keys=master
