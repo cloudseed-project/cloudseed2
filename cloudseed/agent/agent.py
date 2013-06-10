3987037f9a5c15f5cd91018c6aa02faa8f9bd411
Adam Venturella <aventurella@gmail.com>
2013-06-10 07:45:59 -0700

    log message

--- cloudseed/agent/agent.py
+++ cloudseed/agent/agent.py
@@ -20,7 +20,7 @@ def salt_master_events():
     log = logging.getLogger('cloudseed_events')
     log_target = logging.FileHandler('/tmp/cloudseed_events.log')
     log.addHandler(log_target)
-
+    log.debug('Starting Cloudseed Reactor')
     for data in event.iter_events():
 
         pprint(data)
