7f99699985b6cf37a4bbe435c390fd0980b5ea39
Adam Venturella <aventurella@gmail.com>
2013-06-12 13:44:03 -0700

    default tag for events fired within cloudseed: 'cloudseed'

--- cloudseed/utils/events.py
+++ cloudseed/utils/events.py
@@ -5,7 +5,7 @@ from salt.utils.event import SaltEvent
 log = logging.getLogger(__name__)
 
 
-def fire_event(data, tag=None):
+def fire_event(data, tag='cloudseed'):
     event = SaltEvent('master', '/var/run/salt/master')
     log.debug('Sending salt event %s with tag \'%s\'', data, tag)
     event.fire_event(data, tag)
