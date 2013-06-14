c3b7ad62f910804f4d62ba70210ad4f3eb2d22cf
Adam Venturella <aventurella@gmail.com>
2013-06-14 05:22:29 -0700

    added vagrant initialization

--- cloudseed/utils/salt.py
+++ cloudseed/utils/salt.py
@@ -1,8 +1,11 @@
+from __future__ import absolute_import
 import os
+# from saltcloud.utils import gen_keys
 from cloudseed.utils import ssh
 from cloudseed.utils import env
 from .writers import write_string
 from .filesystem import mkdirs
+# from .filesystem import read_file
 
 
 def master_salt_call_highstate():
@@ -24,6 +27,12 @@ def highstate(minion_id=None, grain=None):
     elif grain:
         ssh.sudo(client, 'salt -G \'%s\' state.highstate' % grain)
 
+
+# def create_default_vagrant_folders(prefix=''):
+#     mkdirs(
+#         # default vagrant overrides
+#         os.path.join(prefix, 'vagrant'),
+#         )
 
 def create_default_salt_folders(prefix=''):
     '''
@@ -47,9 +56,22 @@ def create_default_salt_folders(prefix=''):
         )
 
 
+# def create_default_vagrant_files(prefix=''):
+#     '''
+#     Create the default salt configuration files.
+#     '''
+
+#     path_prefix = os.path.join(prefix, 'vagrant')
+#     create_vagrant_minion_config(path_prefix)
+#     create_vagrant_minion_keys(path_prefix)
+#     create_vagrant_vagrantfile()
+#     create_vagrant_bootstrap(path_prefix)
+
+
 def create_default_salt_files(prefix='', data=None):
     '''
     Create the default salt configuration files.
+    data param is currently unused.
     '''
     data = {} if data is None else data
 
@@ -58,6 +80,49 @@ def create_default_salt_files(prefix='', data=None):
     create_salt_cloud_config(path_prefix, data.get('config', None))
     create_salt_cloud_profiles(path_prefix, data.get('profiles', None))
     create_salt_cloud_providers(path_prefix, data.get('providers', None))
+
+
+# def create_vagrant_vagrantfile(prefix=''):
+#     current_path = os.path.join(os.path.dirname(__file__), '../')
+#     current_path = os.path.normpath(current_path)
+
+#     cloudseed_deploy_path = os.path.join(current_path, 'deploy')
+#     vagrantfile = read_file(os.path.join(cloudseed_deploy_path, 'Vagrantfile'))
+
+#     filename = os.path.join(prefix, 'Vagrantfile')
+#     write_string(filename, vagrantfile)
+
+
+# def create_vagrant_bootstrap(prefix=''):
+#     current_path = os.path.join(os.path.dirname(__file__), '../')
+#     current_path = os.path.normpath(current_path)
+
+#     cloudseed_deploy_path = os.path.join(current_path, 'deploy')
+#     bootstrap = read_file(os.path.join(cloudseed_deploy_path, 'bootstrap-salt.sh'))
+
+#     filename = os.path.join(prefix, 'bootstrap-salt.sh')
+#     write_string(filename, bootstrap)
+
+
+# def create_vagrant_minion_keys(prefix):
+#     pem, pub = gen_keys()
+#     filename_pem = os.path.join(prefix, 'minion.pem')
+#     filename_pub = os.path.join(prefix, 'minion.pub')
+
+#     write_string(filename_pem, pem)
+#     write_string(filename_pub, pub)
+
+
+# def create_vagrant_minion_config(prefix='', data=None):
+#     data = '''id: seed-minion.pub
+# master: localhost
+# grains:
+#   roles:
+#     - <role>
+
+# '''
+#     filename = os.path.join(prefix, 'minion')
+#     write_string(filename, data)
 
 
 def create_salt_cloud_profiles(prefix='', data=None):
---
+++ cloudseed/utils/vagrant.py
@@ -0,0 +1,75 @@
+from __future__ import absolute_import
+import os
+import subprocess
+from saltcloud.utils import gen_keys
+from .writers import write_string
+from .filesystem import mkdirs
+from .filesystem import read_file
+
+
+def create_default_vagrant_folders(prefix=''):
+    mkdirs(
+        # default vagrant overrides
+        os.path.join(prefix, 'vagrant'),
+        )
+
+
+def create_default_vagrant_files(prefix=''):
+    '''
+    Create the default salt configuration files.
+    '''
+
+    path_prefix = os.path.join(prefix, 'vagrant')
+    create_vagrant_minion_config(path_prefix)
+    create_vagrant_minion_keys(path_prefix)
+    create_vagrant_vagrantfile()
+    create_vagrant_bootstrap(path_prefix)
+
+
+def create_vagrant_vagrantfile(prefix=''):
+    # TODO handle error if vagrant is not installed.
+    p = subprocess.Popen('vagrant init', shell=True)
+    p.wait()
+
+    current_path = os.path.join(os.path.dirname(__file__), '../')
+    current_path = os.path.normpath(current_path)
+
+    cloudseed_deploy_path = os.path.join(current_path, 'deploy')
+    vagrantfile = read_file(os.path.join(cloudseed_deploy_path, 'Vagrantfile'))
+
+    filename = os.path.join(prefix, 'Vagrantfile')
+    write_string(filename, vagrantfile)
+
+
+def create_vagrant_bootstrap(prefix=''):
+    current_path = os.path.join(os.path.dirname(__file__), '../')
+    current_path = os.path.normpath(current_path)
+
+    cloudseed_deploy_path = os.path.join(current_path, 'deploy')
+
+    bootstrap = read_file(
+        os.path.join(cloudseed_deploy_path, 'bootstrap-salt.sh'))
+
+    filename = os.path.join(prefix, 'bootstrap-salt.sh')
+    write_string(filename, bootstrap)
+
+
+def create_vagrant_minion_keys(prefix):
+    pem, pub = gen_keys()
+    filename_pem = os.path.join(prefix, 'minion.pem')
+    filename_pub = os.path.join(prefix, 'minion.pub')
+
+    write_string(filename_pem, pem)
+    write_string(filename_pub, pub)
+
+
+def create_vagrant_minion_config(prefix='', data=None):
+    data = '''id: seed-minion.pub
+master: localhost
+grains:
+  roles:
+    - <role>
+
+'''
+    filename = os.path.join(prefix, 'minion')
+    write_string(filename, data)
