f1e24c4a435910bccb90d9d0ffc0be5673933bef
Daniel Smith <dsmith@blitzagency.com>
2014-06-09 16:02:21 -0700

    Updates Vagrantfile for --name support

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ packages = [
 
 setup(
     name='cloudseed',
-    version='0.0.1',
+    version='0.0.2',
     description='Cloudseed',
     long_description=readme,
     author='Adam Venturella <aventurella@gmail.com>, Daniel Smith <dsmith@blitzagency.com>',
